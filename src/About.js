960b93a07c06908284e699f465e4ecd8e380c470
Hritvik Dadhich <69057115+Hritvik16000@users.noreply.github.com>
2024-06-05 14:34:47 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -8,12 +8,10 @@ export default function About(){
             <div className="card bg-light text-light m-3 border-0">
             <div className="card-header border-0 about"><h1>About Me</h1></div>
   <div className="card-body bg-light text-dark about-intro">
-    <p className="card-text text-center m-5">I'm Hritvik Dadhich, a versatile developer and data analyst with a solid foundation in both Computer Science Engineering and Data Science, backed by a Master’s degree. 
-        My skill set spans the MERN stack, Python, SQL, data visualization, and Deep Learning. I have a knack for unraveling insights from complex datasets, demonstrated through successful ventures like FutureGaming.io. 
-        I thrive on crafting scalable web solutions and am eager to bring my expertise to your team.</p>
+    <p className="card-text text-center m-5">I'm Hritvik Dadhich, a versatile developer and data analyst with a solid foundation in both Computer Science Engineering and Data Science, backed by a Master’s degree. My skill set spans the MERN stack, Python, SQL, data visualization, and Deep Learning. I have a knack for unraveling insights from complex datasets, demonstrated through successful ventures like FutureGaming.io. I thrive on crafting scalable web solutions and am eager to bring my expertise to your team.</p>
   </div>
   <div className="resume">
-  <a  href="https://drive.google.com/file/d/1YIdrynt2oOqHdPAq61fvoaQlTcM5w5Pi/view?usp=sharing"
+  <a  href="https://drive.google.com/file/d/1wFuVKRsBL8-AHUdvw3lGaRYT0-eJ8R2i/view"
   target="_blank" rel="noreferrer">
                                 <IconButton >
                                     <InsertDriveFileIcon color="secondary"/>
