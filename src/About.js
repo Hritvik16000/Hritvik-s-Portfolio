c5c1000e47e950135bb2f647f49c2273c044f5e9
Hritvik Dadhich <69057115+Hritvik16000@users.noreply.github.com>
2024-06-05 21:44:15 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -8,7 +8,10 @@ export default function About(){
             <div className="card bg-light text-light m-3 border-0">
             <div className="card-header border-0 about"><h1>About Me</h1></div>
   <div className="card-body bg-light text-dark about-intro">
-    <p className="card-text text-center m-5">I'm Hritvik Dadhich, a versatile developer and data analyst with a solid foundation in both Computer Science Engineering and Data Science, backed by a Master’s degree. My skill set spans the MERN stack, Python, SQL, data visualization, and Deep Learning. I have a knack for unraveling insights from complex datasets, demonstrated through successful ventures like FutureGaming.io. I thrive on crafting scalable web solutions and am eager to bring my expertise to your team.</p>
+    <p className="card-text text-center m-5">I'm Hritvik Dadhich,Data Analyst with a Master’s in Data Science and a background in Computer Science Engineering, specializing in Python, SQL, data visualization, and deep learning. Proven expertise in analyzing large datasets to derive meaningful patterns and actionable insights. Successfully led FutureGaming.io, 
+                                            enhancing user experiences through scalable web applications. Proficient in C++, JavaScript, Solidity, React.js, Next.js, Express.js, Node.js, and MongoDB.Strong analytical skills in machine learning, NLP, statistical analysis, and predictive analytics. Advanced data visualization capabilities with Tableau, Matplotlib,
+                                            Seaborn, and Plotly. Demonstrated success in projects like the NBA All-Star Prediction Model and InSight-Loom, a sentiment analysis application. 
+                                            Continuously exploring innovative approaches in data analysis, blockchain technology, and secure file-sharing solutions. Seeking opportunities to contribute to data-driven decision-making and strategic initiatives in a dynamic organization.</p>
   </div>
   <div className="resume">
   <a  href="https://drive.google.com/file/d/1wFuVKRsBL8-AHUdvw3lGaRYT0-eJ8R2i/view"
